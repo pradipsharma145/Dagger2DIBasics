4665a9471e390468549ea443edde1281878a8fe3
Pradip <pradipsharma145@gmail.com>
2021-09-07 18:01:33 +0530

    add extra comments

--- app/src/main/java/com/dhruhesh/dagger2dibasics/MainActivity.kt
+++ app/src/main/java/com/dhruhesh/dagger2dibasics/MainActivity.kt
@@ -8,17 +8,19 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
 
+    //  private lateinit var mCar: Car  //  DO, Construction Injection declaration
+
     @Inject
-    lateinit var mCar: Car
+    lateinit var mCar: Car  //  DO, Field Injection declaration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val carComponent = DaggerCarComponent.create()
-        carComponent.inject(this)
+        carComponent.inject(this)   //  DO, for Field Injection
 
-        //  mCar = carComponent.car
+        //  mCar = carComponent.car //  DO, for constructor Injection
         mCar.drive()
     }
 }
